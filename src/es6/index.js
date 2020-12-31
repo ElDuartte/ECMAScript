bbf4743652af36b1c3d8978aadb48b049d181efb
elduartte <el@duartte.co>
2020-12-31 19:25:24 +0100

    es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -9,7 +9,6 @@ function newFunction(name, age, country) {
 function newfunction2(name = "Oscar", age = 32, country = "ES") {
   console.log(name, age, country);
 }
-
 newfunction2();
 newfunction2("Ricardo", "23", "CO");
 
@@ -17,5 +16,51 @@ let hello = "Hello";
 let world = "World";
 let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
+//es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem =
+  "Quiero escribir una frase epica \n" + "otra frase epica que necesitamos";
+//es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  name: "Oscar",
+  age: "32",
+  country: "ES",
+};
+
+console.log(person.name, person.age, person.country);
+
+//es6
+let { name, age, country } = person;
+console.log(name, country, age);
+
+let team1 = ["Oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Yesica", "Camila"];
+
+let education = ["David", ...team1, ...team2];
+
+console.log(education);
+
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = "global Let";
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = "b";
+//no va a funcionar por que la variable es una 
+//constante la cual NO puede cambiar su valor inicial
+a = "a";
+console.log(a);
