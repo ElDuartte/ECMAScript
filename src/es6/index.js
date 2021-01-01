00e9052ab2d1667d3e8e5bcd0017c21ce06b5b25
elduartte <el@duartte.co>
2021-01-01 18:20:41 +0100

    todos los cambios de ES6

--- src/es6/index.js
+++ src/es6/index.js
@@ -60,7 +60,80 @@ console.log(education);
 console.log(globalVar);
 
 const a = "b";
-//no va a funcionar por que la variable es una 
+//no va a funcionar por que la variable es una
 //constante la cual NO puede cambiar su valor inicial
 a = "a";
 console.log(a);
+
+let name = "Juan";
+let age = 21;
+//es5
+obj = { name: name, age: age };
+//es6
+obj2 = { name, age };
+console.log(obj2);
+
+const names = [
+  { name: "Juan", age: 21 },
+  { name: "Paco", age: 27 },
+];
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+});
+
+listOfNames2 = names.map((item) => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {};
+
+const listOfNames4 = (name) => {};
+
+const square = (num) => num * num;
+
+//promesas
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("hey!!");
+    } else {
+      reject("Ups!!");
+    }
+  });
+};
+
+helloPromise()
+  .then((response) => console.log(response))
+  .then(() => console.log("Hola"))
+  .catch((error) => console.log(error));
+
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import { hello } from './module.js';
+hello();
+
+function* helloWorld() {
+  if (true) {
+    yield "Hello, ";
+  }
+  if (true) {
+    yield "World";
+  }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
