b896579b91a737430e137b4e2b0f439f7c68fa62
elduartte <el@duartte.co>
2021-01-01 18:27:10 +0100

    todos los cambios de ES7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 8, 4, 7];
+
+if (numbers.includes(9)) {
+  console.log("Si esta el valor 7");
+} else {
+  console.log("No se encuentra.");
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
